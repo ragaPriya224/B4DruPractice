6f885cc128311ab350aea24dc95eafc0f01922e8
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-08-18 18:22:28 +0530

    Update ForLoopDemo.java

--- javapractice/b4DruCare/src/b4DruCare/ForLoopDemo.java
+++ javapractice/b4DruCare/src/b4DruCare/ForLoopDemo.java
@@ -4,8 +4,8 @@ public class ForLoopDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i<5;i++){    
-			System.out.println("helloooooooooooooo" +i);
+		for(int count = 0; count < 5;count++){    
+			System.out.println("helloooooooooooooo" +count);
 		} 
 	}
 
