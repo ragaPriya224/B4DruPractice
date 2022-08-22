3ec4f030de58c514fc1accfb80b8e963d20e3021
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-08-22 18:23:40 +0530

    Add files via upload

---
+++ javapractice/collectionDemo/LinkedHashDemo.java
@@ -0,0 +1,17 @@
+package b4DruCare.collectionDemo;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet foodList = new LinkedHashSet();
+		foodList.add("dosa");
+		foodList.add("vada");
+		foodList.add("idli");
+		foodList.add("vada");
+		foodList.add(1);
+		System.out.println(foodList);
+	}
+
+}
