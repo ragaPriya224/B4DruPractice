460e8ce6d86d868fb77885f0c07ce328e9c52791
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-10-06 11:49:49 +0530

    Add files via upload

--- movie-rating-service/src/main/java/com/drucare/movieratingservice/RatingResourceController.java
+++ movie-rating-service/src/main/java/com/drucare/movieratingservice/RatingResourceController.java
@@ -7,28 +7,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/ratingsdata")
+//@RestController
+//@RequestMapping("/ratingsdata")
 public class RatingResourceController {
 	
-	@RequestMapping("/{movieId}")
-	public Rating getRating(@PathVariable String movieId) {
-		
-		return new Rating( movieId,4);
-	}
-
-//	we need to pass userid, and return movie Id's
-	@RequestMapping("/users/{userId}")
-	public UserRating getUserRating(@PathVariable String userId) {
-		List<Rating>  ratingList = Arrays.asList(new Rating("123",4),new Rating("5678", 3));
-//		return ratingList;
-		UserRating userRating = new UserRating();
-		userRating.setUserRating(ratingList);
-		return userRating;
-	}
-	@RequestMapping("/hello")
-	public String sayHello() {
-		return "hello";
-	}
+//	@RequestMapping("/{movieId}")
+//	public Rating getRating(@PathVariable String movieId) {
+//		
+//		return new Rating( movieId,4);
+//	}
+//
+////	we need to pass userid, and return movie Id's
+//	@RequestMapping("/users/{userId}")
+//	public UserRating getUserRating(@PathVariable String userId) {
+//		List<Rating>  ratingList = Arrays.asList(new Rating("123",4),new Rating("5678", 3));
+////		return ratingList;
+//		UserRating userRating = new UserRating();
+//		userRating.setUserRating(ratingList);
+//		return userRating;
+//	}
+//	@RequestMapping("/hello")
+//	public String sayHello() {
+//		return "hello";
+//	}
 	
 }
--- movie-rating-service/src/main/java/com/drucare/movieratingservice/controller/RatingsDataController.java
+++ movie-rating-service/src/main/java/com/drucare/movieratingservice/controller/RatingsDataController.java
@@ -1,11 +1,15 @@
 package com.drucare.movieratingservice.controller;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.drucare.movieratingservice.model.Rating;
+import com.drucare.movieratingservice.model.UserRating;
 
 @RestController
 public class RatingsDataController {
@@ -13,5 +17,15 @@ public class RatingsDataController {
 	@RequestMapping("/ratingsdata/{movieId}")
 	public Rating getRating(@PathVariable String movieId){
 		return new Rating(movieId,4);
+	}
+	
+
+	@RequestMapping("/ratingsdata/user/{userId}")
+	public UserRating getUserRating(@PathVariable String userId){
+		List<Rating> ratingsList = Arrays.asList(new Rating("445",1),
+				new Rating("123",3));
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratingsList);
+		return userRating;
 	}
 }
