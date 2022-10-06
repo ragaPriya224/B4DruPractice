ad4f9840f17afdeca2e6447cb74fff20b31d12e5
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-10-06 11:48:18 +0530

    Add files via upload

--- movie-catalogue-service/src/main/java/com/druPractice/moviecatalogueservice/MovieCatalogueServiceApplication.java
+++ movie-catalogue-service/src/main/java/com/druPractice/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -2,6 +2,8 @@ package com.druPractice.moviecatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
@@ -9,5 +11,9 @@ public class MovieCatalogueServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
 	}
+	@Bean// whoever autowires will get this single instance  //producer 
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	} 
 
 }
--- movie-catalogue-service/src/main/java/com/druPractice/moviecatalogueservice/controller/MovieCatalogueController.java
+++ movie-catalogue-service/src/main/java/com/druPractice/moviecatalogueservice/controller/MovieCatalogueController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +14,23 @@ import org.springframework.web.client.RestTemplate;
 import com.druPractice.moviecatalogueservice.model.Catalogue;
 import com.druPractice.moviecatalogueservice.model.Movie;
 import com.druPractice.moviecatalogueservice.model.Rating;
+import com.druPractice.moviecatalogueservice.model.UserRating;
 
 @RestController
 public class MovieCatalogueController {
 
+	@Autowired  //consumer like 
+	RestTemplate restTemplate;
 
 	@RequestMapping("/catalog/{userId}")
 	public List<Catalogue> getCatalogue(@PathVariable String userId){
 		//		return Collections.singletonList(new Catalogue("spidy","saving earth",4));
-
-		List<Rating> ratingsList = Arrays.asList(new Rating("123",4),
-				new Rating("5678",3));
-
-		RestTemplate restTemplate = new RestTemplate();
-	
-
-		return ratingsList.stream()
+		//hardcoded ratingsdata 
+//		List<Rating> ratingsList = Arrays.asList(new Rating("123",4),
+//				new Rating("5678",3));
+		UserRating userRating = restTemplate.getForObject("http://localhost:8013/ratingsdata/user/33",UserRating.class);
+		
+		return userRating.getUserRating().stream()
 				.map(rating -> {
 					Movie movie = restTemplate.getForObject("http://localhost:8012/movies/4",Movie.class);
 					return new Catalogue(movie.getName(),"test",rating.getRating());
